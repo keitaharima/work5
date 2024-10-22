13546bdee9cd5c709a99cd373e6786e53d21d680
keitaharima <e1b22074@oit.ac.jp>
2024-10-22 13:58:13 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team2/kaizi/work5/Work5Application.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.work5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work5Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team2/kaizi/work5/Work5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.work5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
